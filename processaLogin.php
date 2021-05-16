0d78e18356fd3b480304863eef380339110ce07a
Marcus Santos <marcus.vinnicius02@gmail.com>
2021-05-16 01:48:58 -0300

    Criado a verificaçao de tamanho para a senha

--- processaLogin.php
+++ processaLogin.php
@@ -8,6 +8,13 @@
     $senha = mysqli_real_escape_string($conexao, $_POST['senha']);
     $senha = md5($senha);
 
+    $qtdChar = strlen($senha)
+    
+
+    if ($qtdChar < 8 ) {
+        echo("A senha deve ser maior que 8 caracteres")
+        session_destroy();
+    }
     //Buscar na tabela usuario o usuário que corresponde com os dados digitado no formulário
     $query = "SELECT * FROM usuarios WHERE email = '$usuario' AND senha = '$senha' LIMIT 1";
     $resultado_usuario = mysqli_query($conexao, $query);
